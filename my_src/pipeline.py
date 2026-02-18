dcd41b32c7c92aa26e11a7f71d74b40bbc4f080a
977ADAM <977team.95@gmail.com>
2026-02-18 17:52:59 +0300

    up

--- my_src/pipeline.py
+++ my_src/pipeline.py
@@ -3,7 +3,7 @@
 # =========================
 import pandas as pd
 import numpy as np
-from catboost import CatBoostRegressor
+from catboost import CatBoostRegressor, cv
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
@@ -14,65 +14,72 @@ except ImportError:
     from config import config
     from schema import FEATURE_SCHEMA
 
+def main():
+    # =========================
+    # 2. Загрузка данных
+    # =========================
+    df = pd.read_csv(config.dataset)
 
-# =========================
-# 2. Загрузка данных
-# =========================
-df = pd.read_csv(config.dataset)
+    # =========================
+    # 3. Подготовка данных
+    # =========================
 
-# =========================
-# 3. Подготовка данных
-# =========================
+    # Убираем ID (они не несут полезной информации)
+    df = df.drop(columns=FEATURE_SCHEMA.drop_columns)
 
-# Убираем ID (они не несут полезной информации)
-df = df.drop(columns=FEATURE_SCHEMA.drop_columns)
+    # Целевая переменная
+    y = df[config.target]
 
-# Целевая переменная
-y = df[config.target]
+    # Признаки
+    X = df.drop(columns=[config.target])
 
-# Признаки
-X = df.drop(columns=[config.target])
+    # Категориальные признаки
+    cat_features = FEATURE_SCHEMA.categorical
 
-# Категориальные признаки
-cat_features = FEATURE_SCHEMA.categorical
+    # =========================
+    # 4. Разделение на train/test
+    # =========================
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=config.test_size, random_state=config.random_state
+    )
 
-# =========================
-# 4. Разделение на train/test
-# =========================
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=config.test_size, random_state=config.random_state
-)
+    # =========================
+    # 5. Обучение модели
+    # =========================
+    model = CatBoostRegressor(
+        iterations=config.iterations,
+        depth=config.depth,
+        learning_rate=config.learning_rate,
+        loss_function=config.loss_function,
+        eval_metric=config.eval_metric,
+        random_seed=config.random_seed,
+        verbose=config.verbose
+    )
 
-# =========================
-# 5. Обучение модели
-# =========================
-model = CatBoostRegressor(
-    iterations=config.iterations,
-    depth=config.depth,
-    learning_rate=config.learning_rate,
-    loss_function=config.loss_function,
-    eval_metric=config.eval_metric,
-    random_seed=config.random_seed,
-    verbose=config.verbose
-)
+    model.fit(
+        X_train,
+        y_train,
+        cat_features=cat_features,
+        eval_set=(X_test, y_test),
+        early_stopping_rounds=config.early_stopping_rounds
+    )
 
-model.fit(
-    X_train,
-    y_train,
-    cat_features=cat_features,
-    eval_set=(X_test, y_test),
-    early_stopping_rounds=config.early_stopping_rounds
-)
+    # =========================
+    # 6. Оценка качества
+    # =========================
+    y_pred = model.predict(X_test)
 
-# =========================
-# 6. Оценка качества
-# =========================
-y_pred = model.predict(X_test)
+    mae = mean_absolute_error(y_test, y_pred)
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+    r2 = r2_score(y_test, y_pred)
 
-mae = mean_absolute_error(y_test, y_pred)
-rmse = np.sqrt(mean_squared_error(y_test, y_pred))
-r2 = r2_score(y_test, y_pred)
+    print(f"MAE: {mae:.6f}")
+    print(f"RMSE: {rmse:.6f}")
+    print(f"R2: {r2:.4f}")
 
-print(f"MAE: {mae:.6f}")
-print(f"RMSE: {rmse:.6f}")
-print(f"R2: {r2:.4f}")
+
+    model.save_model("model.pkl")
+
+
+if __name__ == "__main__":
+    main()
--- my_src/streamlit.py
+++ my_src/streamlit.py
@@ -1,18 +1,24 @@
+import pandas as pd
+from catboost import CatBoostRegressor
 import streamlit as st
 try:
     from .config import config
 except ImportError:
     from config import config
 
+df = pd.read_csv(config.dataset)
 
 st.caption(f"Model version: {config.version}")
 st.divider()
 
+model = CatBoostRegressor()
+model.load_model("model.cbm")
+
 banner_type = st.selectbox(
-    'Тип баннера'
+    'Тип баннера', options=df["Тип баннера"].unique()
 )
 divace_type = st.selectbox(
-    "Тип устройства"
+    "Тип устройства", options=df["Тип устройства"].unique()
 )
 
 
